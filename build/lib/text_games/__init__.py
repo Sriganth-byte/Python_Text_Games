9c49e13aafb55771775d3cdc2d21df420d5d1b9e
sriganth12112005 <attitudeling12112001@gmail.com>
2024-12-26 17:53:57 +0530

    Refactor module structure and add new games

--- build/lib/text_games/__init__.py
+++ build/lib/text_games/__init__.py
@@ -1,15 +1,17 @@
-from .games import (
-    hangman,
-    number_guesser,
-    rock_paper_scissors,
-    riddle_game,
-    word_scramble,
-)
+from .hangman import run_hangman
+from .number_guesser import run_number_guesser
+from .rock_paper_scissors import run_rock_paper_scissors
+from .riddle_game import run_riddle_game
+from .word_scrambler import run_word_scrambler
+from .tic_tac_toe import run_tic_tac_toe
+from .snake_game import run_snake_game
 
 __all__ = [
-    "hangman",
-    "number_guesser",
-    "rock_paper_scissors",
-    "riddle_game",
-    "word_scramble",
+    "run_hangman",
+    "run_number_guesser",
+    "run_rock_paper_scissors",
+    "run_riddle_game",
+    "run_word_scrambler",
+    "run_tic_tac_toe",
+    "run_snake_game",
 ]
